8b6ecc504e9e6f064300c23faca7f7c794bc8586
Rodrigo <rodrigojungermourao@gmail.com>
2026-09-15 12:27:56 -0300

    Adiciona rollback seguro contra erro de constraint UNIQUE

--- scraper.py
+++ scraper.py
@@ -58,11 +58,8 @@ def coletar_promocoes():
     db = SessionLocal()
 
     urls = [
-        "https://www.melhoresdestinos.com.br/feed",
         "https://passageirodeprimeira.com/feed/?post_type=post",
-        "https://www.melhorescartoes.com.br/feed",
-        "https://pontospravoar.com/feed/",
-        "https://mestredasmilhas.com.br/feed/"
+        
     ]
 
     headers = {
@@ -141,4 +138,3 @@ def coletar_promocoes():
             continue
 
     db.close()
-    
