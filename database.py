9b8e0c54af28f5e2068258d5755d332c47e1bd90
Rodrigo <rodrigojungermourao@gmail.com>
2026-09-15 11:42:47 -0300

    Padroniza modelo do banco de dados

--- database.py
+++ database.py
@@ -1,25 +1,21 @@
-from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./promocoes.db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
-
 class Promocao(Base):
-  __tablename__ = "promocoes"
+    __tablename__ = "promocoes"
 
-  id = Column(Integer, primary_key=True, index=True)
-  titulo = Column(String, nullable=False)
-  link = Column(String, nullable=False)
-  programa = Column(String, default="Geral")
-  imagem = Column(String, nullable=True)  # link da capa
-  data_coleta = Column(DateTime, default=datetime.utcnow)
-
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String, nullable=False)
+    link = Column(String, unique=True, index=True, nullable=False)
+    programa = Column(String, nullable=True)
+    imagem = Column(String, nullable=True)
 
 Base.metadata.create_all(bind=engine)
